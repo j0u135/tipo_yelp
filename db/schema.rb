cfb767b9f217448e39fa9356734d56460497fe90
j0u135 <j0u135.uam@gmail.com>
2015-11-07 16:37:39 +0000

    Genera la migración para agregar el campo image al objeto restaurant.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107051232) do
+ActiveRecord::Schema.define(version: 20151107163616) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151107051232) do
     t.integer  "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
