c7b6472aa7bf51ba81c21d120c0ed30422a26393
j0u135 <j0u135.uam@gmail.com>
2015-11-09 03:55:42 +0000

    Retira de vistas y controlador los accesos  a index y show de Review por ser innecesarios.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
-  
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
@@ -30,8 +19,8 @@ class ReviewsController < ApplicationController
     
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to '#', notice: 'Review was successfully created.' }
+        format.json { render '#', status: :created, location: @review }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -44,8 +33,8 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
+        format.html { redirect_to '#', notice: 'Review was successfully updated.' }
+        format.json { render '#', status: :ok, location: @review }
       else
         format.html { render :edit }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -58,7 +47,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to '#', notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/views/reviews/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @review, :id, :raitng, :comment, :created_at, :updated_at
