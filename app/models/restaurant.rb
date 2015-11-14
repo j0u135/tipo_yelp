49dabef2bfed1a92e32876e98d3302297232b31f
j0u135 <j0u135.uam@gmail.com>
2015-11-14 02:36:18 +0000

    Modifica la interfaz de inicio para un mejor aspecto.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,10 @@ class Restaurant < ActiveRecord::Base
   
   mount_uploader :image, ImageUploader
   
+  def average
+    if self.reviews.count > 0
+      self.reviews.average(:raitng).round(2)
+    end
+  end
+  
 end
